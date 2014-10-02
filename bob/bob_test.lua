7af0776fa2167e35a0823f28c5c0ab67c05bee12
ryanplusplus <ryanplusplus@gmail.com>
2014-10-01 20:11:55 -0400

    Used whitespace consistent with (most of) the other exercises.  Also replaced double quotes with single quotes per the style guide.

--- bob/bob_test.lua
+++ bob/bob_test.lua
@@ -1,37 +1,40 @@
-local bob = require("bob")
+local bob = require('bob')
 
-describe("Bob", function()
-  it("stating something", function()
+describe('Bob', function()
+
+  it('stating something', function()
     local result = bob.hey('Tom-ay-to, tom-aaaah-to.')
     assert.are.equals('Whatever', result)
   end)
-  it("shouting", function () 
+
+  it('shouting', function ()
       local result = bob.hey('WATCH OUT!')
       assert.are.equals('Whoa, chill out!', result)
   end)
 
-  it("asking a question", function () 
+  it('asking a question', function ()
       local result = bob.hey('Does this cryogenic chamber make me look fat?')
       assert.are.equals('Sure', result)
   end)
 
-  it("talking forcefully", function () 
-      local result = bob.hey("Let's go make out behind the gym!")
+  it('talking forcefully', function ()
+      local result = bob.hey('Let's go make out behind the gym!')
       assert.are.equals('Whatever', result)
   end)
 
-  it("shouting numbers", function () 
+  it('shouting numbers', function ()
       local result = bob.hey('1, 2, 3 GO!')
       assert.are.equals('Whoa, chill out!', result)
   end)
 
-  it("shouting with special characters", function () 
+  it('shouting with special characters', function ()
       local result = bob.hey('ZOMG THE %^*@#$(*^ ZOMBIES ARE COMING!!11!!1!')
       assert.are.equals('Whoa, chill out!', result)
   end)
 
-  it("silence", function () 
+  it('silence', function ()
       local result = bob.hey('')
       assert.are.equals('Fine, be that way.', result)
   end)
+
 end)
