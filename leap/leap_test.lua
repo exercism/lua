ea3b8e6e804eca23cdc97ed5d4ed1c3aff66f768
ryanplusplus <ryanplusplus@gmail.com>
2014-10-18 10:46:30 -0400

    Formatted for consistency

--- leap/leap_test.lua
+++ leap/leap_test.lua
@@ -1,19 +1,21 @@
 local isLeapYear = require('./leap')
 
-describe('Year', function() 
-  it('a known leap year', function() 
+describe('Year', function()
+
+  it('a known leap year', function()
     assert.is_true(isLeapYear(1996))
   end)
 
-  it('any old year', function() 
+  it('any old year', function()
     assert.is_false(isLeapYear(1997))
   end)
 
   it('turn of the 20th century', function()
-    assert.is_false(isLeapYear(1900)) 
+    assert.is_false(isLeapYear(1900))
   end)
 
   it('turn of the 21st century', function()
-    assert.is_true(isLeapYear(2400)) 
+    assert.is_true(isLeapYear(2400))
   end)
+
 end)
