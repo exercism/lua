2ae18464032bcd96a67bb95680b94fbb60944b10
Aarti Parikh <aarti@sachmanya.com>
2014-11-17 10:33:02 -0800

    Scrabble Score exercise

---
+++ scrabble-score/scrabble-score_test.lua
@@ -0,0 +1,27 @@
+local score = require('./scrabble-score')
+
+describe('Scrabble', function()
+    it("scores an empty word as zero", function()
+        assert.are.equal(0, score(""))
+    end)
+
+    it("scores a null as zero", function()
+        assert.are.equal(0, score(null))
+    end)
+
+    it("scores a very short word", function()
+        assert.are.equal(1, score("a"))
+    end)
+
+    it("scores the word by the number of letters", function()
+        assert.are.equal(6, score("street"))
+    end)
+
+    it("scores more complicated words with more", function()
+        assert.are.equal(22, score("quirky"))
+    end)
+
+    it("scores case insensitive words", function()
+        assert.are.equal(20, score("MULTIBILLIONAIRE"))
+    end)
+end)
