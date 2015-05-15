9a1a44cc839c45ea768c4c9c0017d751ceea6d58
Travis Yoder <trayoda@gmail.com>
2015-05-15 12:58:41 -0600

    updates hamming test and example to match hamming definition

--- hamming/hamming_test.lua
+++ hamming/hamming_test.lua
@@ -6,6 +6,10 @@ describe('Hamming', function ()
      assert.are.equals(0, compute('A', 'A'))
   end)
 
+  it('long identical strands', function ()
+    assert.are.equals(0, compute('GGACTGA', 'GGACTGA'))
+  end)
+
   it('complete hamming distance of for single nucleotide strand', function ()
      assert.are.equals(1, compute('A', 'G'))
   end)
@@ -22,12 +26,12 @@ describe('Hamming', function ()
      assert.are.equals(1, compute('GGACG', 'GGTCG'))
   end)
 
-  it('ignores extra length on first strand when longer', function ()
-     assert.are.equals(0, compute('AAAG', 'AAA'))
+  it('non unique character in first strand', function ()
+    assert.are.equals(1, compute('AGA', 'AGG'))
   end)
 
-  it('ignores extra length on other strand when longer', function ()
-     assert.are.equals(0, compute('AAA', 'AAAG'))
+  it('non unique character in second strand', function ()
+    assert.are.equals(1, compute('AGG', 'AGA'))
   end)
 
   it('large hamming distance', function ()
@@ -36,6 +40,10 @@ describe('Hamming', function ()
 
   it('hamming distance in very long strand', function ()
      assert.are.equals(9, compute('GGACGGATTCTG', 'AGGACGGATTCT'))
+  end)
+
+  it('empty strands', function ()
+    assert.are.equals(0, compute('', ''))
   end)
 
 end)
