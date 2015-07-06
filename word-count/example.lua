46d396b85f29e73b98434d0b1eda15ca4e60fac6
Travis Yoder <trayoda@gmail.com>
2015-07-06 16:00:00 -0600

    changes word count expectations from strings to integers

--- word-count/example.lua
+++ word-count/example.lua
@@ -5,17 +5,16 @@ function Words:new(phrase)
   return setmetatable({phrase = phrase:lower()}, self)
 end
 
- 
+
 function Words:count()
   local iter = string.gmatch(self.phrase, "%w+")
   local result = {}
   for element in iter do
     if result[element] == nil then
-      result[element] = "1"
+      result[element] = 1
     else
-      local new_count = tonumber(result[element]) + 1
-      result[element] = tostring(new_count)  
-    end  
+      result[element] = result[element] + 1
+    end
   end
   return result
 end
