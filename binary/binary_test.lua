fd7fd5a92e13dc2d624ff2349b005ae125c764d6
Elias Tandel Barrionovo <elias.tandel@gmail.com>
2015-07-09 09:53:32 -0300

    Remove unnecessary class on binary.

--- binary/binary_test.lua
+++ binary/binary_test.lua
@@ -4,50 +4,51 @@ describe('binary', function()
 
   it('1 is decimal 1', function()
     local expected = 1
-    local result = Binary(1).toDecimal()
+    local result = Binary.toDecimal('1')
     assert.are.equals(expected, result)
   end)
 
   it('10 is decimal 2', function()
     local expected = 2
-    local result = Binary('10').toDecimal()
+    local result = Binary.toDecimal('10')
     assert.are.equals(expected, result)
   end)
 
   it('11 is decimal 3', function()
     local expected = 3
-    local result = Binary('11').toDecimal()
+    local result = Binary.toDecimal('11')
     assert.are.equals(expected, result)
   end)
 
   it('100 is decimal 4', function()
     local expected = 4
-    local result = Binary('100').toDecimal()
+    local result = Binary.toDecimal('100')
     assert.are.equals(expected, result)
   end)
 
   it('1001 is decimal 9', function()
     local expected = 9
-    local result = Binary('1001').toDecimal()
+    local result = Binary.toDecimal('1001')
     assert.are.equals(expected, result)
   end)
 
   it('11010 is decimal 26', function()
     local expected = 26
-    local result = Binary('11010').toDecimal()
+    local result = Binary.toDecimal('11010')
     assert.are.equals(expected, result)
   end)
 
   it('10001101000 is decimal 1128', function()
     local expected = 1128
-    local result = Binary('10001101000').toDecimal()
+    local result = Binary.toDecimal('10001101000')
     assert.are.equals(expected, result)
   end)
 
   it('carrot is decimal 0', function()
     local expected = 0
-    local result = Binary('carrot').toDecimal()
+    local result = Binary.toDecimal('carrot')
     assert.are.equals(expected, result)
   end)
 
 end)
+
--- binary/example.lua
+++ binary/example.lua
@@ -1,18 +1,18 @@
-function Binary(input) 
-  return {
-    toDecimal = function() 
-      if (string.match(input,"%D")) then 
-        return 0
-      end  
-      local str = string.reverse(input)
-      local decimal = 0
-      for i = 1, #str do
-          local c = str:sub(i,i)
-          decimal = decimal + c * (2^(i-1))
-      end
-      return decimal
-    end   
-  }
+local function toDecimal(input)
+  if (string.match(input,"%D")) then
+    return 0
+  end
+  local str = string.reverse(input)
+  local decimal = 0
+  for i = 1, #str do
+      local c = str:sub(i,i)
+      decimal = decimal + c * (2^(i-1))
+  end
+  return decimal
 end
 
-return Binary
+
+return {
+  toDecimal = toDecimal
+}
+
