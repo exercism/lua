3daba7985b3c20d9bb5a319c1a5c42aa1792d1a3
Elias Tandel Barrionovo <elias.tandel@gmail.com>
2015-07-09 09:36:58 -0300

    Remove unnecessary class on word-count.

--- word-count/word-count_test.lua
+++ word-count/word-count_test.lua
@@ -1,39 +1,39 @@
-local Words = require('words')
+local word_count = require('words').word_count
 
 describe('Words', function()
 
   it('counts one word', function()
-    local result = Words:new('word'):count()
+    local result = word_count('word')
     local expected = { word = 1 }
     assert.are.same(expected, result)
   end)
 
   it('counts one of each', function()
-    local result = Words:new('one of each'):count()
+    local result = word_count('one of each')
     local expected = { one = 1, of = 1, each = 1 }
     assert.are.same(expected, result)
   end)
 
   it('counts multiple occurrences', function()
-    local result = Words:new('one fish two fish red fish blue fish'):count()
+    local result = word_count('one fish two fish red fish blue fish')
     local expected = { one= 1, fish = 4, two = 1, red = 1, blue = 1 }
     assert.are.same(expected, result)
   end)
 
   it('ignores punctuation', function()
-    local result = Words:new('car : carpet as java : javascript!!&@$%^&'):count()
+    local result = word_count('car : carpet as java : javascript!!&@$%^&')
     local expected = { car = 1, carpet = 1, as = 1, java = 1, javascript = 1 }
     assert.are.same(expected, result)
   end)
 
   it('includes numbers', function()
-    local result = Words:new('testing, 1, 2 testing'):count()
+    local result = word_count('testing, 1, 2 testing')
     local expected = { testing = 2, ['1'] = 1, ['2'] = 1 }
     assert.are.same(expected, result)
   end)
 
   it('normalizes case', function()
-    local result = Words:new('go Go GO'):count()
+    local result = word_count('go Go GO')
     local expected = { go = 3 }
     assert.are.same(expected, result)
   end)
