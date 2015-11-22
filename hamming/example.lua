2a3eb37afde652542eed8a8192647b3ee9f21ea0
Ryan Hartlage <ryanplusplus@gmail.com>
2015-11-22 15:35:36 -0500

    Corrected indexing issue and style inconsistencies in Hamming example

--- hamming/example.lua
+++ hamming/example.lua
@@ -2,9 +2,9 @@ Hamming = {}
 
 function Hamming.compute(a,b)
   local distance = 0
-  for i=0, a:len() do
-    if (a:sub(i,i) ~= b:sub(i,i) ) then
-      distance= distance+1
+  for i = 1, a:len() do
+    if (a:sub(i, i) ~= b:sub(i, i)) then
+      distance = distance + 1
     end
   end
   return distance
