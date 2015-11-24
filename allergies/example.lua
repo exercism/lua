6d67ebcf3bd42937ec83bf2af8b05ca466004f20
Ryan Hartlage <ryanplusplus@gmail.com>
2015-11-23 19:45:55 -0500

    Added allergies problem

---
+++ allergies/example.lua
@@ -0,0 +1,32 @@
+local allergens = {
+  'eggs',
+  'peanuts',
+  'shellfish',
+  'strawberries',
+  'tomatoes',
+  'chocolate',
+  'pollen',
+  'cats'
+}
+
+local function list(score)
+  local results = {}
+  for i, allergen in ipairs(allergens) do
+    if bit32.band(score, 1 << (i - 1)) > 0 then
+      table.insert(results, allergen)
+    end
+  end
+  return results
+end
+
+local function allergic_to(score, which)
+  for _, allergen in ipairs(list(score)) do
+    if allergen == which then return true end
+  end
+  return false
+end
+
+return {
+  list = list,
+  allergic_to = allergic_to
+}
