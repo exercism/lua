53f89d836345857db3038718d843347fcc31dadb
Ryan Hartlage <ryanplusplus@gmail.com>
2015-11-23 22:06:43 -0500

    Added alphametics exercise

---
+++ alphametics/alphametics_spec.lua
@@ -0,0 +1,29 @@
+local alphametics = require('alphametics')
+
+describe('alphametics', function()
+
+  it('should solve short puzzles', function()
+    local actual = alphametics.solve('I + BB == ILL')
+    local expected = { I = 1, B = 9, L = 0 }
+    assert.same(expected, actual)
+  end)
+
+  it('should solve long puzzles', function()
+    local actual = alphametics.solve('SEND + MORE == MONEY')
+    local expected = { S = 9, E = 5, N = 6, D = 7, M = 1, O = 0, R = 8,Y = 2 }
+    assert.same(expected, actual)
+  end)
+
+  it('should solve puzzles with multiplication', function()
+    local actual = alphametics.solve('IF * DR == DORI')
+    local expected = { I = 8, F = 2, D = 3, R = 9, O = 1 }
+    assert.same(expected, actual)
+  end)
+
+  it('should solve puzzles with any boolean expression', function()
+    local actual = alphametics.solve('PI * R ^ 2 == AREA')
+    local expected = { P = 9, I = 6, R = 7, A = 4, E = 0 }
+    assert.same(expected, actual)
+  end)
+
+end)
---
+++ alphametics/example.lua
@@ -0,0 +1,86 @@
+function generate_combinations(a, n, offset)
+  if n > (#a - offset) then
+    return
+  end
+
+  if n == 0 then
+    coroutine.yield({})
+    return
+  end
+
+  for v in combinations(a, n - 1, offset + 1) do
+    table.insert(v, 1, a[1 + offset])
+    coroutine.yield(v)
+  end
+
+  for v in combinations(a, n, offset + 1) do
+    coroutine.yield(v)
+  end
+end
+
+function combinations(a, n, offset)
+  return coroutine.wrap(function() generate_combinations(a, n, offset or 0) end)
+end
+
+function generate_permutations(a, n)
+  if n == 0 then
+    coroutine.yield(a)
+  else
+    for i = 1, n do
+      a[n], a[i] = a[i], a[n]
+      generate_permutations(a, n - 1)
+      a[n], a[i] = a[i], a[n]
+    end
+  end
+end
+
+function permutations(a)
+  return coroutine.wrap(function () generate_permutations(a, #a) end)
+end
+
+local function unique_letters(s)
+  local letter_set = {}
+  s:gsub('%a', function(l)
+    letter_set[l] = true
+  end)
+
+  local letters = {}
+  for l in pairs(letter_set) do
+    table.insert(letters, l)
+  end
+
+  return letters
+end
+
+local function populate_letter_map(letter_map, letters, values)
+  for i = 1, #letters do
+    letter_map[letters[i]] = values[i]
+  end
+end
+
+local function evaluates_true(solution)
+  return loadstring('return ' .. solution)()
+end
+
+local function has_no_leading_zeros(solution)
+  return not (solution:find('^0%d+') or solution:find('%D0%d+'))
+end
+
+local function solve(puzzle)
+  local letters = unique_letters(puzzle)
+  local letter_map = {}
+
+  for c in combinations({ 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 }, #letters) do
+    for p in permutations(c) do
+      populate_letter_map(letter_map, letters, p)
+      local solution = puzzle:gsub('%a', letter_map)
+      if evaluates_true(solution) and has_no_leading_zeros(solution) then
+        return letter_map
+      end
+    end
+  end
+end
+
+return {
+  solve = solve
+}
