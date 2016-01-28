3b992e9f662d975428a2caf29bac1cf4a55e969c
Ryan Hartlage <ryanplusplus@gmail.com>
2016-01-27 19:08:52 -0500

    Added benchmark test to ensure an efficient implementation for large n

--- exercises/nth-prime/nth-prime_spec.lua
+++ exercises/nth-prime/nth-prime_spec.lua
@@ -2,6 +2,12 @@ local nth = require('nth-prime')
 
 describe('nth-prime', function()
 
+  local function benchmark(f)
+    local start = os.clock()
+    f()
+    return os.clock() - start
+  end
+
   it('should give 2 as the first prime', function()
     assert.equal(2, nth(1))
   end)
@@ -16,6 +22,14 @@ describe('nth-prime', function()
 
   it('should be able to calculate the nth prime for large n', function()
     assert.equal(104743, nth(10001))
+  end)
+
+  it('should be efficient for large n', function()
+    local execution_time = benchmark(function()
+      nth(10001)
+    end)
+
+    assert(execution_time < 1, 'should take less than a second to execute')
   end)
 
   it('should raise an error for n <= 0', function()
