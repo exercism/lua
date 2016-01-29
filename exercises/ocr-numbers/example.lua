6d7baa7f93a2ffa8d696b8047aeff35e03a13f22
Ryan Hartlage <ryanplusplus@gmail.com>
2016-01-28 20:15:17 -0500

    Added OCR exercise

---
+++ exercises/ocr-numbers/example.lua
@@ -0,0 +1,95 @@
+local lookup = {
+  [
+    ' _ ' ..
+    '| |' ..
+    '|_|' ..
+    '   '
+  ] = '0',
+  [
+    '   ' ..
+    '  |' ..
+    '  |' ..
+    '   '
+  ] = '1',
+  [
+    ' _ ' ..
+    ' _|' ..
+    '|_ ' ..
+    '   '
+  ] = '2',
+  [
+    ' _ ' ..
+    ' _|' ..
+    ' _|' ..
+    '   '
+  ] = '3',
+  [
+    '   ' ..
+    '|_|' ..
+    '  |' ..
+    '   '
+  ] = '4',
+  [
+    ' _ ' ..
+    '|_ ' ..
+    ' _|' ..
+    '   '
+  ] = '5',
+  [
+    ' _ ' ..
+    '|_ ' ..
+    '|_|' ..
+    '   '
+  ] = '6',
+  [
+    ' _ ' ..
+    '  |' ..
+    '  |' ..
+    '   '
+  ] = '7',
+  [
+    ' _ ' ..
+    '|_|' ..
+    '|_|' ..
+    '   '
+  ] = '8',
+  [
+    ' _ ' ..
+    '|_|' ..
+    ' _|' ..
+    '   '
+  ] = '9',
+}
+
+local row_pattern = ('[^\n]+\n'):rep(4) .. '*'
+
+local function lines(s)
+  local lines = {}
+  for line in s:gmatch('[^\n]+') do
+    table.insert(lines, line)
+  end
+  return lines
+end
+
+local function parse(row)
+  local result = ''
+  local lines = lines(row)
+  for i = 1, #lines[1], 3 do
+    local character = ''
+    for line = 1, 4 do
+      character = character .. lines[line]:sub(i, i + 2)
+    end
+    result = result .. (lookup[character] or '?')
+  end
+  return result
+end
+
+return {
+  convert = function(s)
+    local results = {}
+    for row in s:gmatch(row_pattern) do
+      table.insert(results, parse(row))
+    end
+    return table.concat(results, ',')
+  end
+}
---
+++ exercises/ocr-numbers/ocr-numbers_spec.lua
@@ -0,0 +1,163 @@
+local ocr = require('ocr-numbers')
+
+describe('ocr-numbers', function()
+
+  it('should recognize zero', function()
+    local actual = ocr.convert(
+      ' _ \n' ..
+      '| |\n' ..
+      '|_|\n' ..
+      '   '
+    )
+    assert.equal('0', actual)
+  end)
+
+  it('should recognize one', function()
+    local actual = ocr.convert(
+      '   \n' ..
+      '  |\n' ..
+      '  |\n' ..
+      '   '
+    )
+    assert.equal('1', actual)
+  end)
+
+  it('should recognize two', function()
+    local actual = ocr.convert(
+      ' _ \n' ..
+      ' _|\n' ..
+      '|_ \n' ..
+      '   '
+    )
+    assert.equal('2', actual)
+  end)
+
+  it('should recognize three', function()
+    local actual = ocr.convert(
+      ' _ \n' ..
+      ' _|\n' ..
+      ' _|\n' ..
+      '   '
+    )
+    assert.equal('3', actual)
+  end)
+
+  it('should recognize four', function()
+    local actual = ocr.convert(
+      '   \n' ..
+      '|_|\n' ..
+      '  |\n' ..
+      '   '
+    )
+    assert.equal('4', actual)
+  end)
+
+  it('should recognize five', function()
+    local actual = ocr.convert(
+      ' _ \n' ..
+      '|_ \n' ..
+      ' _|\n' ..
+      '   '
+    )
+    assert.equal('5', actual)
+  end)
+
+  it('should recognize six', function()
+    local actual = ocr.convert(
+      ' _ \n' ..
+      '|_ \n' ..
+      '|_|\n' ..
+      '   '
+    )
+    assert.equal('6', actual)
+  end)
+
+  it('should recognize seven', function()
+    local actual = ocr.convert(
+      ' _ \n' ..
+      '  |\n' ..
+      '  |\n' ..
+      '   '
+    )
+    assert.equal('7', actual)
+  end)
+
+  it('should recognize eight', function()
+    local actual = ocr.convert(
+      ' _ \n' ..
+      '|_|\n' ..
+      '|_|\n' ..
+      '   '
+    )
+    assert.equal('8', actual)
+  end)
+
+  it('should recognize nine', function()
+    local actual = ocr.convert(
+      ' _ \n' ..
+      '|_|\n' ..
+      ' _|\n' ..
+      '   '
+    )
+    assert.equal('9', actual)
+  end)
+
+  it('should identify garble', function()
+    local actual = ocr.convert(
+      '   \n' ..
+      '| |\n' ..
+      '| |\n' ..
+      '   '
+    )
+    assert.equal('?', actual)
+  end)
+
+  it('should recognize two numbers on the same line', function()
+    local actual = ocr.convert(
+      '    _ \n' ..
+      '  || |\n' ..
+      '  ||_|\n' ..
+      '      '
+    )
+    assert.equal('10', actual)
+  end)
+
+  it('should recognize multiple numbers on the same line', function()
+    local actual = ocr.convert(
+      '    _  _     _  _  _  _  _  _ \n' ..
+      '  | _| _||_||_ |_   ||_||_|| |\n' ..
+      '  ||_  _|  | _||_|  ||_| _||_|\n' ..
+      '                              '
+    )
+    assert.equal('1234567890', actual)
+  end)
+
+  it('should identify garble mixed into a line of numbers', function()
+    local actual = ocr.convert(
+      '       _     _           _ \n' ..
+      '  |  || |  || |     || || |\n' ..
+      '  |  | _|  ||_|  |  ||_||_|\n' ..
+      '                           '
+    )
+    assert.equal('11?10?1?0', actual)
+  end)
+
+  it('should convert multiple lines of numbers as comma separated values', function()
+    local actual = ocr.convert(
+      '    _  _ \n' ..
+      '  | _| _|\n' ..
+      '  ||_  _|\n' ..
+      '         \n' ..
+      '    _  _ \n' ..
+      '|_||_ |_ \n' ..
+      '  | _||_|\n' ..
+      '         \n' ..
+      ' _  _  _ \n' ..
+      '  ||_||_|\n' ..
+      '  ||_| _|\n' ..
+      '         '
+    )
+    assert.equal('123,456,789', actual)
+  end)
+
+end)
