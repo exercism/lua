5a76db4184e5a66ee1cc98daacb72d79a8cd330d
Ryan Hartlage <ryanplusplus@gmail.com>
2016-02-06 10:17:14 -0500

    Added Pig Latin exercise

---
+++ exercises/pig-latin/example.lua
@@ -0,0 +1,21 @@
+local function translate(word)
+  local function begins_with_any_of(patterns)
+    for _, pattern in ipairs(patterns) do
+      if word:find('^' .. pattern) then return true end
+    end
+  end
+
+  if begins_with_any_of({ 'a', 'e', 'i', 'o', 'u', 'yt', 'xr' }) then
+    return word .. 'ay'
+  elseif begins_with_any_of({ '[^aeiou]qu', 'thr', 'sch' }) then
+    return word:sub(4) .. word:sub(1, 3) .. 'ay'
+  elseif begins_with_any_of({ 'ch', 'qu', 'th' }) then
+    return word:sub(3) .. word:sub(1, 2) .. 'ay'
+  else
+    return word:sub(2) .. word:sub(1, 1) .. 'ay'
+  end
+end
+
+return function(phrase)
+  return phrase:gsub('%a+', function(word) return translate(word) end)
+end
