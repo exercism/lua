8b98979b78f5db3bd2e963941fa08c1e9d532a92
Ryan Hartlage <ryanplusplus@gmail.com>
2016-02-17 21:08:28 -0500

    Fixed indentation and unintended global

--- exercises/bob/example.lua
+++ exercises/bob/example.lua
@@ -1,22 +1,22 @@
-bob = {}
+local bob = {}
 
-local function shouting(say) 
-    return string.upper(say) == say
+local function shouting(say)
+  return string.upper(say) == say
 end
 
-local function nothing(say) 
-    return say == nil or string.len(say) == 0
+local function nothing(say)
+  return say == nil or string.len(say) == 0
 end
 
 local function question(say)
-    return string.sub(say, -1) == '?'
+  return string.sub(say, -1) == '?'
 end
 
 function bob.hey( say )
-  if (nothing(say))  then return 'Fine, be that way.' end
-  if (shouting(say)) then return "Whoa, chill out!" end
-  if (question(say)) then return "Sure" end
+  if nothing(say)  then return 'Fine, be that way.' end
+  if shouting(say) then return "Whoa, chill out!" end
+  if question(say) then return "Sure" end
   return "Whatever"
-end  
+end
 
 return bob
