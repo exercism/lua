b688bcc216c4fa57e387bb82713b908f87c72e2f
Ryan Hartlage <ryanplusplus@gmail.com>
2016-02-27 09:18:38 -0500

    Added Run Length Encoding exercise

---
+++ exercises/run-length-encoding/example.lua
@@ -0,0 +1,22 @@
+return {
+  encode = function(s)
+    local result = ''
+    local i = 1
+    while i <= #s do
+      local c = s:sub(i, i)
+      local _, _, run = s:find('(' .. c .. '+)', i)
+      result = result .. ((#run > 1) and #run or '') .. c
+      i = i + #run
+    end
+    return result
+  end,
+
+  decode = function(s)
+    local result = ''
+    for length, c in s:gmatch('(%d?)(.)') do
+      if length == '' then length = 1 end
+      result = result .. c:rep(length)
+    end
+    return result
+  end
+}
---
+++ exercises/run-length-encoding/run-length-encoding_spec.lua
@@ -0,0 +1,23 @@
+local rle = require('run-length-encoding')
+
+describe('run-length-encoding', function()
+  it('should encode simple strings', function()
+    assert.equal('2A3B4C', rle.encode('AABBBCCCC'))
+  end)
+
+  it('should decode simple strings', function()
+    assert.equal('AABBBCCCC', rle.decode('2A3B4C'))
+  end)
+
+  it('should not encode characters with a run length of 1', function()
+    assert.equal('2AB4CD', rle.encode('AABCCCCD'))
+  end)
+
+  it('should successfully decode characters with a run length of 1', function()
+    assert.equal('AABCCCCD', rle.decode('2AB4CD'))
+  end)
+
+  it('should decode an encoded string back to the original string', function()
+    assert.equal('zzz ZZ  zZ', rle.decode(rle.encode('zzz ZZ  zZ')))
+  end)
+end)
