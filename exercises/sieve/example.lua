9283fff7eda9e291b2e86409a9b837e674f20518
Ryan Hartlage <ryanplusplus@gmail.com>
2016-03-18 21:09:04 -0400

    Refactored to generate primes using a coroutine

--- exercises/sieve/example.lua
+++ exercises/sieve/example.lua
@@ -1,22 +1,19 @@
 return function(n)
-  local candidates = { false }
-  local primes = {}
+  return coroutine.create(function()
+    local is_prime = {}
 
-  for i = 2, n do
-    candidates[i] = true
-  end
+    for i = 2, n do
+      is_prime[i] = true
+    end
 
-  for i = 2, math.sqrt(n) do
-    if candidates[i] then
-      for multiple = i * 2, n, i do
-        candidates[multiple] = false
+    for i = 2, n do
+      if is_prime[i] then
+        coroutine.yield(i)
+
+        for multiple = i * i, n, i do
+          is_prime[multiple] = false
+        end
       end
     end
-  end
-
-  for candidate, is_prime in ipairs(candidates) do
-    if is_prime then table.insert(primes, candidate) end
-  end
-
-  return primes
+  end)
 end
