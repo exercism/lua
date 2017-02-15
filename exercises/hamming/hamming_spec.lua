75f707d076547173cbfceb1a8a0e0498649d8b90
Ryan Hartlage <ryanplusplus@gmail.com>
2017-02-15 13:31:03 -0500

    are.equals => are.equal

--- exercises/hamming/hamming_spec.lua
+++ exercises/hamming/hamming_spec.lua
@@ -2,54 +2,54 @@ local compute = require('hamming').compute
 
 describe('hamming', function ()
   it('no difference between identical strands', function ()
-    assert.are.equals(0, compute('A', 'A'))
+    assert.are.equal(0, compute('A', 'A'))
   end)
 
   it('long identical strands', function ()
-    assert.are.equals(0, compute('GGACTGA', 'GGACTGA'))
+    assert.are.equal(0, compute('GGACTGA', 'GGACTGA'))
   end)
 
   it('complete hamming distance of for single nucleotide strand', function ()
-    assert.are.equals(1, compute('A', 'G'))
+    assert.are.equal(1, compute('A', 'G'))
   end)
 
   it('complete hamming distance of for small strand', function ()
-    assert.are.equals(2, compute('AG', 'CT'))
+    assert.are.equal(2, compute('AG', 'CT'))
   end)
 
   it('small hamming distance', function ()
-    assert.are.equals(1, compute('AT', 'CT'))
+    assert.are.equal(1, compute('AT', 'CT'))
   end)
 
   it('small hamming distance in longer strand', function ()
-    assert.are.equals(1, compute('GGACG', 'GGTCG'))
+    assert.are.equal(1, compute('GGACG', 'GGTCG'))
   end)
 
   it('non unique character in first strand', function ()
-    assert.are.equals(1, compute('AGA', 'AGG'))
+    assert.are.equal(1, compute('AGA', 'AGG'))
   end)
 
   it('non unique character in second strand', function ()
-    assert.are.equals(1, compute('AGG', 'AGA'))
+    assert.are.equal(1, compute('AGG', 'AGA'))
   end)
 
   it('large hamming distance', function ()
-    assert.are.equals(4, compute('GATACA', 'GCATAA'))
+    assert.are.equal(4, compute('GATACA', 'GCATAA'))
   end)
 
   it('hamming distance in very long strand', function ()
-    assert.are.equals(9, compute('GGACGGATTCTG', 'AGGACGGATTCT'))
+    assert.are.equal(9, compute('GGACGGATTCTG', 'AGGACGGATTCT'))
   end)
 
   it('empty strands', function ()
-    assert.are.equals(0, compute('', ''))
+    assert.are.equal(0, compute('', ''))
   end)
 
   it('disallow first strand longer', function ()
-    assert.are.equals(-1, compute('AATG', 'AAA'))
+    assert.are.equal(-1, compute('AATG', 'AAA'))
   end)
 
   it('disallow second strand longer', function ()
-    assert.are.equals(-1, compute('ATA', 'AGTG'))
+    assert.are.equal(-1, compute('ATA', 'AGTG'))
   end)
 end)
