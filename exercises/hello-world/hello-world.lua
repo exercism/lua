d0b0e557d19a8310b5c393faca48b740dc6c8460
Rob Phoenix <rob@robphoenix.com>
2017-02-21 10:58:24 +0000

    hello-world: add stub file

---
+++ exercises/hello-world/hello-world.lua
@@ -0,0 +1,18 @@
+-- This is a "stub" file.  It's a little start on your solution.
+-- It's not a complete solution though; you have to write some code.
+
+-- Table to be returned by the hello-world module.
+local hello_world = {}
+
+-- Add the hello() function to the table returned by this module.
+function hello_world.hello()
+  -- Write some code here to pass the test suite.
+
+  -- When you have a working solution, REMOVE ALL THE STOCK COMMENTS.
+  -- They're here to help you get started but they only clutter a finished solution.
+  -- If you leave them in, reviewers will protest!
+  return ''
+end
+
+-- Return the hello_world table to make it accessible as a module.
+return hello_world
--- exercises/hello-world/hello-world_spec.lua
+++ exercises/hello-world/hello-world_spec.lua
@@ -1,6 +1,7 @@
+-- Require the hello-world module
 local hello_world = require('hello-world')
 
--- Define a module named hello-world. This module should contain a single
+-- Define a module named hello-world. This module should return a single
 -- function named hello that takes no arguments and returns a string.
 
 describe('hello-world', function()
