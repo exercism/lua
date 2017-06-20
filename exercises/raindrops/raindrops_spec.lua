d6af58ee665d04681aed51d22a1d92cfeace0037
Rob Phoenix <rob@robphoenix.com>
2017-06-20 12:18:20 +0100

    raindrops: bring tests in line with canonical data (#205)

--- exercises/raindrops/raindrops_spec.lua
+++ exercises/raindrops/raindrops_spec.lua
@@ -1,43 +1,75 @@
 local raindrops = require('raindrops')
 
 describe('raindrops', function()
-  it('should return the number as a string if it does not have 3, 5, or 7 as a prime factor', function()
+  it('the sound for 1 is 1', function()
     assert.equal('1', raindrops(1))
-    assert.equal('2', raindrops(2))
-    assert.equal('52', raindrops(52))
   end)
 
-  it('should return "Pling" if the number has 3 as a prime factor', function()
+  it('the sound for 3 is Pling', function()
     assert.equal('Pling', raindrops(3))
+  end)
+
+  it('the sound for 5 is Plang', function()
+    assert.equal('Plang', raindrops(5))
+  end)
+
+  it('the sound for 7 is Plong', function()
+    assert.equal('Plong', raindrops(7))
+  end)
+
+  it('the sound for 6 is Pling as it has a factor 3', function()
     assert.equal('Pling', raindrops(6))
+  end)
+
+  it('2 to the power 3 does not make a raindrop sound as 3 is the exponent not the base', function()
+    assert.equal('8', raindrops(8))
+  end)
+
+  it('the sound for 9 is Pling as it has a factor 3', function()
     assert.equal('Pling', raindrops(9))
   end)
 
-  it('should return "Plang" if the number has 5 as a prime factor', function()
-    assert.equal('Plang', raindrops(5))
+  it('the sound for 10 is Plang as it has a factor 5', function()
     assert.equal('Plang', raindrops(10))
-    assert.equal('Plang', raindrops(25))
   end)
 
-  it('should return "Plong" if the number has 7 as a prime factor', function()
+  it('the sound for 14 is Plong as it has a factor 7', function()
     assert.equal('Plong', raindrops(7))
-    assert.equal('Plong', raindrops(14))
-    assert.equal('Plong', raindrops(49))
   end)
 
-  it('should return "PlingPlang" if the number has both 3 and 5 as a prime factors', function()
+  it('the sound for 15 is PlingPlang as it has a factor 3 and 5', function()
     assert.equal('PlingPlang', raindrops(15))
   end)
 
-  it('should return "PlingPlong" if the number has both 3 and 7 as a prime factors', function()
+  it('the sound for 21 is PlingPlong as it has factors 3 and 7', function()
     assert.equal('PlingPlong', raindrops(21))
   end)
 
-  it('should return "PlangPlong" if the number has both 5 and 7 as a prime factors', function()
+  it('the sound for 25 is Plang as it has a factor 5', function()
+    assert.equal('Plang', raindrops(25))
+  end)
+
+  it('the sound for 27 is Pling as it has a factor 3', function()
+    assert.equal('Pling', raindrops(27))
+  end)
+
+  it('the sound for 35 is PlangPlong as it has factors 5 and 7', function()
     assert.equal('PlangPlong', raindrops(35))
   end)
 
-  it('should return "PlingPlangPlong" if the number has 3, 5, and 7 as a prime factors', function()
+  it('the sound for 49 is Plong as it has a factor 7', function()
+    assert.equal('Plong', raindrops(49))
+  end)
+
+  it('the sound for 52 is 52', function()
+    assert.equal('52', raindrops(52))
+  end)
+
+  it('the sound for 105 is PlingPlangPlong as it has factors 3, 5 and 7', function()
     assert.equal('PlingPlangPlong', raindrops(105))
+  end)
+
+  it('the sound for 3125 is Plang as it has a factor 5', function()
+    assert.equal('Plang', raindrops(3125))
   end)
 end)
