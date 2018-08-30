b91bdd8c15ca9aa2ee84104d95c560af6d8f80ac
Scientifica96 <bentbibrachad@gmail.com>
2018-08-30 21:43:24 +0100

    difference-of-squares: renamed square_of_sums to square_of_sum

--- exercises/difference-of-squares/example.lua
+++ exercises/difference-of-squares/example.lua
@@ -1,4 +1,4 @@
-local function square_of_sums(n)
+local function square_of_sum(n)
   local sum = 0
   for i = 1, n do
     sum = sum + i
@@ -15,11 +15,11 @@ local function sum_of_squares(n)
 end
 
 local function difference_of_squares(n)
-  return square_of_sums(n) - sum_of_squares(n)
+  return square_of_sum(n) - sum_of_squares(n)
 end
 
 return {
-  square_of_sums = square_of_sums,
+  square_of_sum = square_of_sum,
   sum_of_squares = sum_of_squares,
   difference_of_squares = difference_of_squares
 }
