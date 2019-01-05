63598b24d8d05253e27640cc1fc93496461a594f
Tom Godkin <thgodkin@gmail.com>
2019-01-05 16:38:23 +0000

    Update pythagorean-triplet to reflect README

--- exercises/pythagorean-triplet/example.lua
+++ exercises/pythagorean-triplet/example.lua
@@ -1,30 +1,15 @@
-local function is_triplet(...)
-  local t = table.pack(...)
-  table.sort(t)
-  return t[1] ^ 2 + t[2] ^ 2 == t[3] ^2
-end
-
-local function triplets_with(options)
-  local function matching_triplet(a, b, c)
-    if is_triplet(a, b, c) then
-      return not options.sum or (options.sum == (a + b + c))
-    end
-  end
-
+return function(sum)
   local triplets = {}
-  for a = options.min_factor or 1, options.max_factor do
-    for b = a + 1, options.max_factor do
-      for c = b + 1, options.max_factor do
-        if matching_triplet(a, b, c) then
-          table.insert(triplets, { a, b, c })
-        end
+
+  for a = 1, sum // 3 - 1 do
+    for b = a, (sum - a) // 2 do
+      local c = sum - a - b
+
+      if a ^ 2 + b ^ 2 == c ^ 2 then
+        table.insert(triplets, { a, b, c})
       end
     end
   end
+
   return triplets
 end
-
-return {
-  is_triplet = is_triplet,
-  triplets_with = triplets_with
-}
--- exercises/pythagorean-triplet/pythagorean-triplet_spec.lua
+++ exercises/pythagorean-triplet/pythagorean-triplet_spec.lua
@@ -1,4 +1,4 @@
-local pythagorean = require('pythagorean-triplet')
+local triplets_with_sum = require('pythagorean-triplet')
 
 describe('pythagorean-triplet', function()
   local function sort(triplets)
@@ -6,37 +6,68 @@ describe('pythagorean-triplet', function()
     return triplets
   end
 
-  describe('is_triplet', function()
-    it('should identify triplets', function()
-      assert.is_true(pythagorean.is_triplet(3, 4, 5))
-      assert.is_true(pythagorean.is_triplet(5, 12, 13))
-    end)
-
-    it('should identify non-triplets', function()
-      assert.is_false(pythagorean.is_triplet(3, 4, 6))
-      assert.is_false(pythagorean.is_triplet(5, 6, 17))
-    end)
-  end)
-
-  describe('triplets_with', function()
-    it('should generate all triplets with a specified maximum factor', function()
+  describe('triplets_with_sum', function()
+    it('finds triplets whose sum is 12', function()
       assert.same(
-        { { 3, 4, 5 },  { 5, 12, 13 }, { 6, 8, 10 }, { 8, 15, 17}, { 9, 12, 15 } },
-        sort(pythagorean.triplets_with{ max_factor = 17 })
+        { { 3, 4, 5 } },
+        sort(triplets_with_sum(12))
       )
     end)
 
-    it('should generate all triplets with a specified minimum and maximum factor', function()
+    it('finds triplets whose sum is 108', function()
       assert.same(
-        { { 6, 8, 10 }, { 9, 12, 15 } },
-        sort(pythagorean.triplets_with{ min_factor = 6, max_factor = 15 })
+        { { 27, 36, 45 } },
+        sort(triplets_with_sum(108))
       )
     end)
 
-    it('should generate all triplets with a specified maximum factor and a specified sum', function()
+    it('finds triplets whose sum is 1000', function()
       assert.same(
-        { { 18, 80, 82 }, { 30, 72, 78 }, { 45, 60, 75 } },
-        sort(pythagorean.triplets_with{ sum = 180, max_factor = 100 })
+        { { 200, 375, 425 } },
+        sort(triplets_with_sum(1000))
+      )
+    end)
+
+    it('finds no triplets whose sum is 1001', function()
+      assert.same({}, triplets_with_sum(1001))
+    end)
+
+    it('finds triplets whose sum is 90', function()
+      assert.same(
+        {
+          { 9, 40, 41 },
+          { 15, 36, 39 },
+        },
+        sort(triplets_with_sum(90))
+      )
+    end)
+
+    it('finds triplets whose sum is 840', function()
+      assert.same(
+        {
+          { 40, 399, 401 },
+          { 56, 390, 394 },
+          { 105, 360, 375 },
+          { 120, 350, 370 },
+          { 140, 336, 364 },
+          { 168, 315, 357 },
+          { 210, 280, 350 },
+          { 240, 252, 348 },
+        },
+        sort(triplets_with_sum(840))
+      )
+    end)
+
+    it('finds triplets whose sum is a large number (30000)', function()
+      assert.same(
+        {
+          { 1200, 14375, 14425 },
+          { 1875, 14000, 14125 },
+          { 5000, 12000, 13000 },
+          { 6000, 11250, 12750 },
+          { 7500, 10000, 12500 },
+        },
+        sort(triplets_with_sum(30000))
       )
     end)
   end)
