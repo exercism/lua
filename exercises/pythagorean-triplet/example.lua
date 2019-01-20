6092e468d2a8cfb65ffed7d7675bb0de13bade2b
Tom Godkin <thgodkin@gmail.com>
2019-01-20 12:41:08 +0000

    pythagorean-triplet: more efficient solution

--- exercises/pythagorean-triplet/example.lua
+++ exercises/pythagorean-triplet/example.lua
@@ -1,13 +1,34 @@
 return function(sum)
+  local function from_a(x)
+    -- Given that:
+    -- (1) a + b + c = p
+    -- (2) a ^ 2 + b ^ 2 = c ^ 2
+    -- (2) c = sqrt(a ^ 2 + b ^ 2)
+    --
+    -- (2) -> (1) a + b + sqrt(a ^ 2 + b ^ 2) = p
+    --
+    -- Rearranging we get:
+    -- b = 0.5 * (p - (pa / (p - a)))
+
+    return (sum - sum * x / (sum - x)) / 2
+  end
+
   local triplets = {}
 
-  for a = 1, sum // 3 - 1 do
-    for b = a, (sum - a) // 2 do
-      local c = sum - a - b
+  for a=1, sum do
+    local b = from_a(a)
 
-      if a ^ 2 + b ^ 2 == c ^ 2 then
-        table.insert(triplets, { a, b, c})
-      end
+    if a > b then
+      break
+    end
+
+    local frac
+    b, frac = math.modf(b)
+    if frac == 0.0 then
+      -- Mathematically from_a might yield non-natural numbers which are then
+      -- truncated by integer division, so here we filter only for integer
+      -- triplets.
+      table.insert(triplets, { a, b, sum - (a + b) })
     end
   end
 
