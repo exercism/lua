6244e0a9a1c0f4996313178c856d9e2f3405be33
Elyashiv <>
2020-07-23 14:29:14 +0300

    upgrade word-count tests

--- exercises/word-count/word-count_spec.lua
+++ exercises/word-count/word-count_spec.lua
@@ -1,3 +1,6 @@
+
+-- problem specification version 1.4.0
+
 local word_count = require('word-count').word_count
 
 describe('word-count', function()
@@ -19,6 +22,18 @@ describe('word-count', function()
     assert.are.same(expected, result)
   end)
 
+  it('handles cramped lists', function()
+    local result = word_count('one,two,three')
+    local expected = { one = 1, two = 1, three = 1}
+    assert.are.same(expected, result)
+  end)
+
+  it('handles expanded lists', function()
+    local result = word_count('one,\ntwo,\nthree')
+    local expected = { one = 1, two = 1, three = 1}
+    assert.are.same(expected, result)
+  end)
+
   it('ignores punctuation', function()
     local result = word_count('car : carpet as java : javascript!!&@$%^&')
     local expected = { car = 1, carpet = 1, as = 1, java = 1, javascript = 1 }
@@ -32,8 +47,38 @@ describe('word-count', function()
   end)
 
   it('normalizes case', function()
-    local result = word_count('go Go GO')
-    local expected = { go = 3 }
+    local result = word_count('go Go GO Stop stop')
+    local expected = { go = 3, stop = 2 }
+    assert.are.same(expected, result)
+  end)
+
+  it('counts with apostrophes', function()
+    local result = word_count("First: don't laugh. Then: don't cry.")
+    local expected = { first = 1, ["don't"] = 2, laugh = 1, ['then'] = 1, cry = 1}
+    assert.are.same(expected, result)
+  end)
+
+  it('counts with quatation', function()
+    local result = word_count("Joe can't tell between 'large' and large.")
+    local expected = { joe = 1, ["can't"] = 1, tell = 1, between = 1, large = 2, ["and"] = 1}
+    assert.are.same(expected, result)
+  end)
+
+  it('counts with substirngs from the beginning', function()
+    local result = word_count("Joe can't tell between app, apple and a.")
+    local expected = { joe = 1, ["can't"] = 1, tell = 1, between = 1, app = 1, apple = 1, ["and"] = 1, a = 1}
+    assert.are.same(expected, result)
+  end)
+
+  it('does not count multiple spaces as a word', function()
+    local result = word_count(' multiple   whitespaces')
+    local expected = {multiple = 1, whitespaces = 1}
+    assert.are.same(expected, result)
+  end)
+
+  it('alternating word separators not detected as a word', function()
+    local result = word_count(",\n,one,\n ,two \n 'three'")
+    local expected = {one = 1, two = 1, three = 1}
     assert.are.same(expected, result)
   end)
 end)
