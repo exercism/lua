0ec227c2d14a4d73623c9e5be33fdda11f78f826
Ryan Hartlage <ryanplusplus@gmail.com>
2023-05-03 08:25:52 -0400

    Synced crypto square with upstream (#383)

--- exercises/practice/crypto-square/crypto-square.lua
+++ exercises/practice/crypto-square/crypto-square.lua
@@ -1,27 +1,5 @@
-local function normalized_plaintext(plaintext)
-
-end
-
-local function size(plaintext)
-
-end
-
-local function segments(plaintext)
-
-end
-
-local function normalized_ciphertext(plaintext)
-
-end
-
-local function ciphertext(plaintext)
-
-end
-
 return {
-  normalized_plaintext = normalized_plaintext,
-  size = size,
-  segments = segments,
-  ciphertext = ciphertext,
-  normalized_ciphertext = normalized_ciphertext
+  ciphertext = function(plaintext)
+
+  end
 }
