a53384951d2233c68273f11713610ae12dbc4874
Eric Willigers <keiravillekode@gmail.com>
2023-10-29 07:48:12 +1100

    Add resistor-color-duo exercise (#398)

---
+++ exercises/practice/resistor-color-duo/resistor-color-duo_spec.lua
@@ -0,0 +1,31 @@
+local rcd = require 'resistor-color-duo'
+
+describe('resistor-color-duo', function()
+  it('Brown and black', function()
+    assert.equal(10, rcd.value({'brown', 'black'}))
+  end)
+
+  it('Blue and grey', function()
+    assert.equal(68, rcd.value({'blue', 'grey'}))
+  end)
+
+  it('Yellow and violet', function()
+    assert.equal(47, rcd.value({'yellow', 'violet'}))
+  end)
+
+  it('White and red', function()
+    assert.equal(92, rcd.value({'white', 'red'}))
+  end)
+
+  it('Orange and orange', function()
+    assert.equal(33, rcd.value({'orange', 'orange'}))
+  end)
+
+  it('Ignore additional colors', function()
+    assert.equal(51, rcd.value({'green', 'brown', 'orange'}))
+  end)
+
+  it('Black and brown, one-digit', function()
+    assert.equal(1, rcd.value({'black', 'brown'}))
+  end)
+end)
