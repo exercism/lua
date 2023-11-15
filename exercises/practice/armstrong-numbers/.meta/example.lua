0db91a754f4261d2a1fe096df58801562f96dc6f
András B Nagy <20251272+BNAndras@users.noreply.github.com>
2023-11-15 09:50:28 -0800

    Add armstrong-numbers (#415)

---
+++ exercises/practice/armstrong-numbers/.meta/example.lua
@@ -0,0 +1,16 @@
+local ArmstrongNumbers = {}
+
+function ArmstrongNumbers.isArmstrongNumber(number)
+    local digits = tostring(number)
+    local pow_special = function(digit) return tonumber(digit) ^ string.len(digits) end
+
+    local sum = 0
+    for i = 1, string.len(digits) do
+        local digit = tonumber(string.sub(digits, i, i))
+        sum = sum + pow_special(digit)
+    end
+
+    return sum == number
+end
+
+return ArmstrongNumbers
