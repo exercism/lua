1518ebb0731bf3376f916b028748057cb62d8f6e
Ryan Hartlage <ryanplusplus@gmail.com>
2023-11-17 08:02:02 -0500

    Last one, I think

--- exercises/practice/armstrong-numbers/armstrong-numbers_spec.lua
+++ exercises/practice/armstrong-numbers/armstrong-numbers_spec.lua
@@ -2,38 +2,38 @@ local ArmstrongNumbers = require 'armstrong-numbers'
 
 describe('armstrong-nunbers', function()
   it('Zero is an Armstrong number', function()
-    assert.is_true(ArmstrongNumbers.isArmstrongNumber(0))
+    assert.is_true(ArmstrongNumbers.is_armstrong_number(0))
   end)
 
   it('Single-digit numbers are Armstrong numbers', function()
-    assert.is_true(ArmstrongNumbers.isArmstrongNumber(5))
+    assert.is_true(ArmstrongNumbers.is_armstrong_number(5))
   end)
 
   it('There are no two-digit Armstrong numbers', function()
-    assert.is_false(ArmstrongNumbers.isArmstrongNumber(10))
+    assert.is_false(ArmstrongNumbers.is_armstrong_number(10))
   end)
 
   it('Three-digit number that is an Armstrong number', function()
-    assert.is_true(ArmstrongNumbers.isArmstrongNumber(153))
+    assert.is_true(ArmstrongNumbers.is_armstrong_number(153))
   end)
 
   it('Three-digit number that is not an Armstrong number', function()
-    assert.is_false(ArmstrongNumbers.isArmstrongNumber(100))
+    assert.is_false(ArmstrongNumbers.is_armstrong_number(100))
   end)
 
   it('Four-digit number that is an Armstrong number', function()
-    assert.is_true(ArmstrongNumbers.isArmstrongNumber(9474))
+    assert.is_true(ArmstrongNumbers.is_armstrong_number(9474))
   end)
 
   it('Four-digit number that is not an Armstrong number', function()
-    assert.is_false(ArmstrongNumbers.isArmstrongNumber(9475))
+    assert.is_false(ArmstrongNumbers.is_armstrong_number(9475))
   end)
 
   it('Seven-digit number that is an Armstrong number', function()
-    assert.is_true(ArmstrongNumbers.isArmstrongNumber(9926315))
+    assert.is_true(ArmstrongNumbers.is_armstrong_number(9926315))
   end)
 
   it('Seven-digit number that is not an Armstrong number', function()
-    assert.is_false(ArmstrongNumbers.isArmstrongNumber(9926314))
+    assert.is_false(ArmstrongNumbers.is_armstrong_number(9926314))
   end)
 end)
