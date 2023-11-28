5f5484eee8c88ec5b6ecfc048cadc0fb29d9b1ee
András B Nagy <20251272+BNAndras@users.noreply.github.com>
2023-11-28 05:11:06 -0800

    Add high-scores (#420)
    * Add high-scores
    
    * Apply code review
    
    * Apply additional code review
    
    * Fix tests and apply formatting

---
+++ exercises/practice/high-scores/.meta/example.lua
@@ -0,0 +1,30 @@
+local function scores(self)
+  return self.values
+end
+
+local function latest(self)
+  return self.values[#self.values]
+end
+
+local function personal_best(self)
+  return math.max(table.unpack(self.values))
+end
+
+local function personal_top_three(self)
+  local values = { table.unpack(self.values) }
+  table.sort(values, function(a, b)
+    return a > b
+  end)
+  local stop = math.min(3, #values)
+  return { table.unpack(values, 1, stop) }
+end
+
+return function(values)
+  return {
+    values = table.unpack(values),
+    scores = scores,
+    latest = latest,
+    personal_best = personal_best,
+    personal_top_three = personal_top_three
+  }
+end
---
+++ exercises/practice/high-scores/high-scores_spec.lua
@@ -0,0 +1,91 @@
+local HighScores = require 'high-scores'
+
+describe('high-scores', function()
+  it('table of scores', function()
+    local values = { 30, 50, 20, 70 }
+    local scores = HighScores { values }
+    local expected = { 30, 50, 20, 70 }
+    assert.are.same(expected, scores:scores())
+  end)
+
+  it('latest score', function()
+    local values = { 100, 0, 90, 30 }
+    local scores = HighScores { values }
+    local expected = 30
+    assert.equal(expected, scores:latest())
+  end)
+
+  it('personal best', function()
+    local values = { 40, 100, 70 }
+    local scores = HighScores { values }
+    local expected = 100
+    assert.equal(expected, scores:personal_best())
+  end)
+
+  it('personal top three from a list of scores', function()
+    local values = { 10, 30, 90, 30, 100, 20, 10, 0, 30, 40, 40, 70, 70 }
+    local scores = HighScores { values }
+    local expected = { 100, 90, 70 }
+    assert.are.same(expected, scores:personal_top_three())
+  end)
+
+  it('personal top highest to lowest', function()
+    local values = { 20, 10, 30 }
+    local scores = HighScores { values }
+    local expected = { 30, 20, 10 }
+    assert.are.same(expected, scores:personal_top_three())
+  end)
+
+  it('personal top when there is a tie', function()
+    local values = { 40, 20, 40, 30 }
+    local scores = HighScores { values }
+    local expected = { 40, 40, 30 }
+    assert.are.same(expected, scores:personal_top_three())
+  end)
+
+  it('personal top when there are less than 3', function()
+    local values = { 30, 70 }
+    local scores = HighScores { values }
+    local expected = { 70, 30 }
+    assert.are.same(expected, scores:personal_top_three())
+  end)
+
+  it('personal top when there is only one', function()
+    local values = { 40 }
+    local scores = HighScores { values }
+    local expected = { 40 }
+    assert.are.same(expected, scores:personal_top_three())
+  end)
+
+  it('latest score after personal top scores', function()
+    local values = { 70, 50, 20, 30 }
+    local scores = HighScores { values }
+    local _ = scores:personal_top_three()
+    local expected = 30
+    assert.equal(expected, scores:latest())
+  end)
+
+  it('scores after personal top scores', function()
+    local values = { 30, 50, 20, 70 }
+    local scores = HighScores { values }
+    local _ = scores:personal_top_three()
+    local expected = { 30, 50, 20, 70 }
+    assert.are.same(expected, scores:scores())
+  end)
+
+  it('latest score after personal best', function()
+    local values = { 20, 70, 15, 25, 30 }
+    local scores = HighScores { values }
+    local _ = scores:personal_best()
+    local expected = 30
+    assert.equal(expected, scores:latest())
+  end)
+
+  it('scores after personal best', function()
+    local values = { 20, 70, 15, 25, 30 }
+    local scores = HighScores { values }
+    local _ = scores:personal_best()
+    local expected = { 20, 70, 15, 25, 30 }
+    assert.are.same(expected, scores:scores())
+  end)
+end)
