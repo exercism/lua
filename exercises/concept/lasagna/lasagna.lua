e50496a50a1621d6ce7e7e04ab67c0474d978c4c
Sebastian Huebner <sebastian@hueb-ner.de>
2024-02-18 16:52:28 +0100

    removed extra new line in lasagna.lua

--- exercises/concept/lasagna/lasagna.lua
+++ exercises/concept/lasagna/lasagna.lua
@@ -2,7 +2,6 @@ local lasagna = {}
 
 -- add a 'oven_time' field to the lasagna table
 
-
 -- returns the remaining minutes based on the actual minutes in the oven
 function lasagna.remaining_oven_time(actual_minutes_in_oven)
   -- remaining_oven_time body
