8040770c9f3046d35f2c3c8a127f683a6c800149
Sebastian Huebner <sebastian@hueb-ner.de>
2024-02-18 16:45:54 +0100

    corrected indentation

--- exercises/concept/lasagna/lasagna.lua
+++ exercises/concept/lasagna/lasagna.lua
@@ -5,19 +5,19 @@ local lasagna = {}
 
 -- returns the remaining minutes based on the actual minutes in the oven
 function lasagna.remaining_oven_time(actual_minutes_in_oven)
-    -- remaining_oven_time body
+  -- remaining_oven_time body
 end
 
 -- calculates and returns the time needed to prepare the lasagna on the amount
 -- of layers
 function lasagna.preparation_time(number_of_layers)
-    -- preparation_time body
+  -- preparation_time body
 end
 
 -- calculates the time elapsed cooking the lasagna - including preparation time
 -- and baking time.
 function lasagna.elapsed_time(number_of_layers, actual_minutes_in_oven)
-    -- elapsed_time body
+  -- elapsed_time body
 end
 
 return lasagna
