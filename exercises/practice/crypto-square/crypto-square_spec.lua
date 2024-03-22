e83afa6ae25d58d1da5ea8482efb0bb352595120
Glenn Jackman <glenn.jackman@gmail.com>
2024-03-22 18:31:51 +0000

    format2

--- exercises/practice/crypto-square/crypto-square_spec.lua
+++ exercises/practice/crypto-square/crypto-square_spec.lua
@@ -31,7 +31,6 @@ describe('crypto-square', function()
 
   it('54 character plaintext results in 7 chunks, the last two with trailing spaces', function()
     assert.are.equal('imtgdvs fearwer mayoogo anouuio ntnnlvt wttddes aohghn  sseoau ',
-                     crypto_square.ciphertext('If man was meant to stay on the ground, god would have given us roots.')
-    )
+                     crypto_square.ciphertext('If man was meant to stay on the ground, god would have given us roots.'))
   end)
 end)
