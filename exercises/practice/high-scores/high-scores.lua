1486147cce1a3b9c50e8823be0c6d1ff499bfb7c
Ryan Hartlage <ryanplusplus@gmail.com>
2024-09-06 21:02:00 -0400

    Improve high scores experience

--- exercises/practice/high-scores/high-scores.lua
+++ exercises/practice/high-scores/high-scores.lua
@@ -1,2 +1,23 @@
+local HighScores = {}
+
+function HighScores:scores()
+
+end
+
+function HighScores:latest()
+
+end
+
+function HighScores:personal_best()
+
+end
+
+function HighScores:personal_top_three()
+
+end
+
 return function(scores)
+  local high_scores = {}
+  setmetatable(high_scores, { __index = HighScores })
+  return high_scores
 end
