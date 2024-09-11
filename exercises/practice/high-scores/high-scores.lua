4deb3e96d169fdf806d78d42dee93cf1cab77fb6
Glenn Jackman <glenn.jackman@gmail.com>
2024-09-11 08:09:33 -0400

    Add a hint into the high-scores stub Discussed in http://forum.exercism.org/t/high-scores-stub/12879

--- exercises/practice/high-scores/high-scores.lua
+++ exercises/practice/high-scores/high-scores.lua
@@ -19,5 +19,8 @@ end
 return function(scores)
   local high_scores = {}
   setmetatable(high_scores, { __index = HighScores })
+
+  -- TODO: store the scores in the high_scores object
+
   return high_scores
 end
