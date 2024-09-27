9acea8c2ac3969adb884e777911a5dca2496ae55
Ryan Hartlage <ryanplusplus@gmail.com>
2024-09-27 14:09:53 -0400

    Apply suggestions from code review

--- exercises/practice/proverb/proverb_spec.lua
+++ exercises/practice/proverb/proverb_spec.lua
@@ -2,26 +2,26 @@ local Proverb = require 'proverb'
 
 describe('proverb', function()
   it('zero pieces', function()
-    strings = {}
-    expected = table.concat({})
+    local strings = {}
+    local expected = table.concat({})
     assert.equal(expected, Proverb.recite(strings))
   end)
 
   it('one piece', function()
-    strings = { 'nail' }
-    expected = table.concat({ "And all for the want of a nail.\n" })
+    local strings = { 'nail' }
+    local expected = table.concat({ "And all for the want of a nail.\n" })
     assert.equal(expected, Proverb.recite(strings))
   end)
 
   it('two pieces', function()
-    strings = { 'nail', 'shoe' }
-    expected = table.concat({ "For want of a nail the shoe was lost.\n", "And all for the want of a nail.\n" })
+    local strings = { 'nail', 'shoe' }
+    local expected = table.concat({ "For want of a nail the shoe was lost.\n", "And all for the want of a nail.\n" })
     assert.equal(expected, Proverb.recite(strings))
   end)
 
   it('three pieces', function()
-    strings = { 'nail', 'shoe', 'horse' }
-    expected = table.concat({
+    local strings = { 'nail', 'shoe', 'horse' }
+    local expected = table.concat({
       "For want of a nail the shoe was lost.\n",
       "For want of a shoe the horse was lost.\n",
       "And all for the want of a nail.\n"
@@ -30,8 +30,8 @@ describe('proverb', function()
   end)
 
   it('full proverb', function()
-    strings = { 'nail', 'shoe', 'horse', 'rider', 'message', 'battle', 'kingdom' }
-    expected = table.concat({
+    local strings = { 'nail', 'shoe', 'horse', 'rider', 'message', 'battle', 'kingdom' }
+    local expected = table.concat({
       "For want of a nail the shoe was lost.\n",
       "For want of a shoe the horse was lost.\n",
       "For want of a horse the rider was lost.\n",
@@ -44,8 +44,8 @@ describe('proverb', function()
   end)
 
   it('four pieces modernized', function()
-    strings = { 'pin', 'gun', 'soldier', 'battle' }
-    expected = table.concat({
+    local strings = { 'pin', 'gun', 'soldier', 'battle' }
+    local expected = table.concat({
       "For want of a pin the gun was lost.\n",
       "For want of a gun the soldier was lost.\n",
       "For want of a soldier the battle was lost.\n",
