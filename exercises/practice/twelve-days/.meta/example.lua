6862ec13650b93883fcf2f97aee99c5cbec2981a
Eric Willigers <keiravillekode@gmail.com>
2024-09-28 13:20:03 +1000

    Generate each verse

--- exercises/practice/twelve-days/.meta/example.lua
+++ exercises/practice/twelve-days/.meta/example.lua
@@ -1,19 +1,40 @@
 local function recite_verse(verse)
-  local lyrics = {
-    'On the first day of Christmas my true love gave to me: a Partridge in a Pear Tree.',
-    'On the second day of Christmas my true love gave to me: two Turtle Doves, and a Partridge in a Pear Tree.',
-    'On the third day of Christmas my true love gave to me: three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.',
-    'On the fourth day of Christmas my true love gave to me: four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.',
-    'On the fifth day of Christmas my true love gave to me: five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.',
-    'On the sixth day of Christmas my true love gave to me: six Geese-a-Laying, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.',
-    'On the seventh day of Christmas my true love gave to me: seven Swans-a-Swimming, six Geese-a-Laying, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.',
-    'On the eighth day of Christmas my true love gave to me: eight Maids-a-Milking, seven Swans-a-Swimming, six Geese-a-Laying, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.',
-    'On the ninth day of Christmas my true love gave to me: nine Ladies Dancing, eight Maids-a-Milking, seven Swans-a-Swimming, six Geese-a-Laying, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.',
-    'On the tenth day of Christmas my true love gave to me: ten Lords-a-Leaping, nine Ladies Dancing, eight Maids-a-Milking, seven Swans-a-Swimming, six Geese-a-Laying, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.',
-    'On the eleventh day of Christmas my true love gave to me: eleven Pipers Piping, ten Lords-a-Leaping, nine Ladies Dancing, eight Maids-a-Milking, seven Swans-a-Swimming, six Geese-a-Laying, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.',
-    'On the twelfth day of Christmas my true love gave to me: twelve Drummers Drumming, eleven Pipers Piping, ten Lords-a-Leaping, nine Ladies Dancing, eight Maids-a-Milking, seven Swans-a-Swimming, six Geese-a-Laying, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.'
+  local ordinals = {
+    'first',
+    'second',
+    'third',
+    'fourth',
+    'fifth',
+    'sixth',
+    'seventh',
+    'eighth',
+    'ninth',
+    'tenth',
+    'eleventh',
+    'twelfth'
   }
-  return lyrics[verse]
+
+  local gifts = {
+    'a Partridge in a Pear Tree.',
+    'two Turtle Doves, and',
+    'three French Hens,',
+    'four Calling Birds,',
+    'five Gold Rings,',
+    'six Geese-a-Laying,',
+    'seven Swans-a-Swimming,',
+    'eight Maids-a-Milking,',
+    'nine Ladies Dancing,',
+    'ten Lords-a-Leaping,',
+    'eleven Pipers Piping,',
+    'twelve Drummers Drumming,'
+  }
+
+  local result = { 'On the', ordinals[verse], 'day of Christmas my true love gave to me:' }
+  for index = verse, 1, -1 do
+    table.insert(result, gifts[index])
+  end
+
+  return table.concat(result, ' ')
 end
 
 local function recite(start_verse, end_verse)
