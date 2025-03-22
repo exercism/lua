9f6159fdd8cef3188375af8bc3470d87536f47ac
Eric Willigers <keiravillekode@gmail.com>
2025-03-22 22:57:25 +1100

    Sync isogram tests

--- exercises/practice/isogram/isogram_spec.lua
+++ exercises/practice/isogram/isogram_spec.lua
@@ -1,32 +1,59 @@
-local is_isogram = require 'isogram'
+local is_isogram = require('isogram')
 
 describe('isogram', function()
-  it('should identify simple isograms', function()
-    assert.is_true(is_isogram('duplicates'))
-    assert.is_true(is_isogram('subdermatoglyphic'))
+  it('empty string', function()
+    assert.is_true(is_isogram(''))
   end)
 
-  it('should identify words with repeated letters as non-isograms', function()
+  it('isogram with only lower case characters', function()
+    assert.is_true(is_isogram('isogram'))
+  end)
+
+  it('word with one duplicated character', function()
     assert.is_false(is_isogram('eleven'))
   end)
 
-  it('should be case insensitive', function()
+  it('word with one duplicated character from the end of the alphabet', function()
+    assert.is_false(is_isogram('zzyzx'))
+  end)
+
+  it('longest reported english isogram', function()
+    assert.is_true(is_isogram('subdermatoglyphic'))
+  end)
+
+  it('word with duplicated character in mixed case', function()
     assert.is_false(is_isogram('Alphabet'))
   end)
 
-  it('should allow punctuations in isograms', function()
+  it('word with duplicated character in mixed case, lowercase first', function()
+    assert.is_false(is_isogram('alphAbet'))
+  end)
+
+  it('hypothetical isogrammic word with hyphen', function()
     assert.is_true(is_isogram('thumbscrew-japingly'))
   end)
 
-  it('should allow repeated punctuation in isograms', function()
-    assert.is_true(is_isogram('Hjelmqvist-Gryb-Zock-Pfund-Wax'))
+  it('hypothetical word with duplicated character following hyphen', function()
+    assert.is_false(is_isogram('thumbscrew-jappingly'))
   end)
 
-  it('should identify non-isogram sentences', function()
-    assert.is_false(is_isogram('the quick brown fox'))
+  it('isogram with duplicated hyphen', function()
+    assert.is_true(is_isogram('six-year-old'))
   end)
 
-  it('should identify isogram sentences', function()
+  it('made-up name that is an isogram', function()
     assert.is_true(is_isogram('Emily Jung Schwartzkopf'))
+  end)
+
+  it('duplicated character in the middle', function()
+    assert.is_false(is_isogram('accentor'))
+  end)
+
+  it('same first and last characters', function()
+    assert.is_false(is_isogram('angola'))
+  end)
+
+  it('word with duplicated character and with two hyphens', function()
+    assert.is_false(is_isogram('up-to-date'))
   end)
 end)
