58374410d67fc87a4556762a75a3cfdb5efb1c21
Eric Willigers <keiravillekode@gmail.com>
2025-07-02 22:06:06 +1000

    sync prime-factors

--- exercises/practice/prime-factors/prime-factors_spec.lua
+++ exercises/practice/prime-factors/prime-factors_spec.lua
@@ -1,47 +1,51 @@
 local prime_factors = require('prime-factors')
 
 describe('prime-factors', function()
-  it('returns an empty array for 1', function()
+  it('no factors', function()
     assert.are.same({}, prime_factors(1))
   end)
 
-  it('factors 2', function()
+  it('prime number', function()
     assert.are.same({ 2 }, prime_factors(2))
   end)
 
-  it('factors 3', function()
+  it('another prime number', function()
     assert.are.same({ 3 }, prime_factors(3))
   end)
 
-  it('factors 4', function()
-    assert.are.same({ 2, 2 }, prime_factors(4))
-  end)
-
-  it('factors 6', function()
-    assert.are.same({ 2, 3 }, prime_factors(6))
-  end)
-
-  it('factors 8', function()
-    assert.are.same({ 2, 2, 2 }, prime_factors(8))
-  end)
-
-  it('factors 9', function()
+  it('square of a prime', function()
     assert.are.same({ 3, 3 }, prime_factors(9))
   end)
 
-  it('factors 27', function()
+  it('product of first prime', function()
+    assert.are.same({ 2, 2 }, prime_factors(4))
+  end)
+
+  it('cube of a prime', function()
+    assert.are.same({ 2, 2, 2 }, prime_factors(8))
+  end)
+
+  it('product of second prime', function()
     assert.are.same({ 3, 3, 3 }, prime_factors(27))
   end)
 
-  it('factors 625', function()
+  it('product of third prime', function()
     assert.are.same({ 5, 5, 5, 5 }, prime_factors(625))
   end)
 
-  it('factors 901255', function()
+  it('product of first and second prime', function()
+    assert.are.same({ 2, 3 }, prime_factors(6))
+  end)
+
+  it('product of primes and non-primes', function()
+    assert.are.same({ 2, 2, 3 }, prime_factors(12))
+  end)
+
+  it('product of primes', function()
     assert.are.same({ 5, 17, 23, 461 }, prime_factors(901255))
   end)
 
-  it('factors 93819012551', function()
+  it('factors include a large prime', function()
     assert.are.same({ 11, 9539, 894119 }, prime_factors(93819012551))
   end)
 end)
