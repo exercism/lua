0f88e9d0d3c65461a4e1c5b27c48e15781a90d45
Eric Willigers <keiravillekode@gmail.com>
2025-07-02 17:38:01 +1000

    format

--- exercises/practice/bob/.meta/spec_generator.lua
+++ exercises/practice/bob/.meta/spec_generator.lua
@@ -5,8 +5,6 @@ return {
     local template = [[
       local result = bob.hey("%s")
       assert.are.equals("%s", result)]]
-    return template:format(
-      case.input.heyBob:gsub("\n", "\\n"):gsub("\r", "\\r"):gsub("\t", "\\t"),
-      case.expected)
+    return template:format(case.input.heyBob:gsub("\n", "\\n"):gsub("\r", "\\r"):gsub("\t", "\\t"), case.expected)
   end
 }
