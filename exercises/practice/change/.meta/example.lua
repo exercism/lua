2ee5f30bafbbfc96719c83352da738cb476752ac
Eric Willigers <keiravillekode@gmail.com>
2025-07-03 11:48:54 +1000

    sync change

--- exercises/practice/change/.meta/example.lua
+++ exercises/practice/change/.meta/example.lua
@@ -1,19 +1,37 @@
 return function(amount, values)
-  local index, sorted_values, result = {}, {}, {}
-
-  for i, v in ipairs(values) do
-    index[v] = i
-    sorted_values[i] = v
+  assert(amount >= 0, "target can't be negative")
+  if amount == 0 then
+    return {}
   end
 
-  table.sort(sorted_values, function(a, b)
-    return b < a
-  end)
-
-  for i, value in ipairs(sorted_values) do
-    result[index[value]] = amount // value
-    amount = amount % value
+  local counts = {}
+  local coins = {}
+  for total = 1, amount do
+    local best_count = amount + 1
+    local best_coin = 0
+    for _, value in ipairs(values) do
+      if value == total then
+        best_count = 1
+        best_coin = value
+      elseif total > value then
+        local count = counts[total - value] + 1
+        if count < best_count then
+          best_count = count
+          best_coin = value
+        end
+      end
+    end
+    table.insert(counts, best_count)
+    table.insert(coins, best_coin)
   end
 
-  return amount == 0 and result or nil
+  assert(counts[amount] <= amount, "can't make target with given coins")
+
+  local result = {}
+  local remaining = amount
+  while remaining > 0 do
+    table.insert(result, coins[remaining])
+    remaining = remaining - coins[remaining]
+  end
+  return result
 end
--- exercises/practice/change/change_spec.lua
+++ exercises/practice/change/change_spec.lua
@@ -1,28 +1,62 @@
 local change = require('change')
 
 describe('change', function()
-  it('should generate the correct change when there is only one type of coin', function()
-    assert.same({ 5 }, change(5, { 1 }))
+  it('change for 1 cent', function()
+    assert.same({ 1 }, change(1, { 1, 5, 10, 25 }))
   end)
 
-  it('should generate the correct change when there are multiple coin types', function()
-    assert.same({ 5, 0 }, change(5, { 1, 10 }))
+  it('single coin change', function()
+    assert.same({ 25 }, change(25, { 1, 5, 10, 25, 100 }))
   end)
 
-  it('should generate the correct change when multiple types of coins are needed', function()
-    assert.same({ 3, 1, 1 }, change(18, { 1, 5, 10 }))
+  it('multiple coin change', function()
+    assert.same({ 5, 10 }, change(15, { 1, 5, 10, 25, 100 }))
   end)
 
-  it('should return nil if it is not possible to make change', function()
-    assert.is_nil(change(3, { 5, 10, 25 }))
+  it('change with lilliputian coins', function()
+    assert.same({ 4, 4, 15 }, change(23, { 1, 4, 15, 20, 50 }))
   end)
 
-  it('should generate the correct change given any coin order', function()
-    assert.same({ 3, 1, 1 }, change(18, { 1, 5, 10 }))
-    assert.same({ 1, 1, 3 }, change(18, { 10, 5, 1 }))
+  it('change with lower elbonia coins', function()
+    assert.same({ 21, 21, 21 }, change(63, { 1, 5, 10, 21, 25 }))
   end)
 
-  it('should generate the correct change for large values with many coins', function()
-    assert.same({ 3, 1, 0, 1, 1 }, change(133, { 1, 5, 10, 25, 100 }))
+  it('large target values', function()
+    assert.same({ 2, 2, 5, 20, 20, 50, 100, 100, 100, 100, 100, 100, 100, 100, 100 },
+                change(999, { 1, 2, 5, 10, 20, 50, 100 }))
+  end)
+
+  it('possible change without unit coins available', function()
+    assert.same({ 2, 2, 2, 5, 10 }, change(21, { 2, 5, 10, 20, 50 }))
+  end)
+
+  it('another possible change without unit coins available', function()
+    assert.same({ 4, 4, 4, 5, 5, 5 }, change(27, { 4, 5 }))
+  end)
+
+  it('a greedy approach is not optimal', function()
+    assert.same({ 10, 10 }, change(20, { 1, 10, 11 }))
+  end)
+
+  it('no coins make 0 change', function()
+    assert.same({}, change(0, { 1, 5, 10, 21, 25 }))
+  end)
+
+  it('error testing for change smaller than the smallest of coins', function()
+    assert.has.error(function()
+      change(3, { 5, 10 })
+    end, "can't make target with given coins")
+  end)
+
+  it('error if no combination can add up to target', function()
+    assert.has.error(function()
+      change(94, { 5, 10 })
+    end, "can't make target with given coins")
+  end)
+
+  it('cannot find negative change values', function()
+    assert.has.error(function()
+      change(-5, { 1, 2, 5 })
+    end, "target can't be negative")
   end)
 end)
