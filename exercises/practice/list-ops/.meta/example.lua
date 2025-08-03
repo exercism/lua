84aea2e2b77cf4f42c4f5d5e4297cfbaa83b9d38
Ryan Hartlage <2488333+ryanplusplus@users.noreply.github.com>
2025-08-03 19:01:05 -0400

    Update list-ops tests

--- exercises/practice/list-ops/.meta/example.lua
+++ exercises/practice/list-ops/.meta/example.lua
@@ -1,19 +1,60 @@
-local function reduce(xs, value, f)
+local function append(xs, ys)
+  local result = {}
   for _, x in ipairs(xs) do
-    value = f(x, value)
+    table.insert(result, x)
+  end
+  for _, y in ipairs(ys) do
+    table.insert(result, y)
+  end
+  return result
+end
+
+local function concat(...)
+  local result = {}
+  for _, list in ipairs({ ... }) do
+    for _, item in ipairs(list) do
+      table.insert(result, item)
+    end
+  end
+  return result
+end
+
+local function length(xs)
+  local count = 0
+  for _ in ipairs(xs) do
+    count = count + 1
+  end
+  return count
+end
+
+local function reverse(xs)
+  local result = {}
+  for i = #xs, 1, -1 do
+    table.insert(result, xs[i])
+  end
+  return result
+end
+
+local function foldl(xs, value, f)
+  for _, x in ipairs(xs) do
+    value = f(value, x)
   end
   return value
 end
 
+local function foldr(xs, value, f)
+  return foldl(reverse(xs), value, f)
+end
+
 local function map(xs, f)
-  return reduce(xs, {}, function(x, acc)
+  return foldl(xs, {}, function(acc, x)
     table.insert(acc, f(x))
     return acc
   end)
 end
 
 local function filter(xs, pred)
-  return reduce(xs, {}, function(x, acc)
+  return foldl(xs, {}, function(acc, x)
     if pred(x) then
       table.insert(acc, x)
     end
@@ -21,4 +62,13 @@ local function filter(xs, pred)
   end)
 end
 
-return { map = map, reduce = reduce, filter = filter }
+return {
+  append = append,
+  concat = concat,
+  length = length,
+  reverse = reverse,
+  map = map,
+  foldl = foldl,
+  foldr = foldr,
+  filter = filter
+}
