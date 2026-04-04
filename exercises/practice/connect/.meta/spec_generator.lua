3610a7736b18fbc91438d9346460764faf41085e
keiravillekode <keiravillekode@gmail.com>
2026-04-04 21:53:24 +1100

    Sync connect tests (#641)
    * Sync connect tests
    
    * feedback

---
+++ exercises/practice/connect/.meta/spec_generator.lua
@@ -0,0 +1,27 @@
+local map = function(t, f)
+  local mapped = {}
+  for i, v in ipairs(t) do
+    mapped[i] = f(v)
+  end
+  return mapped
+end
+
+local function render_board(board)
+  return table.concat(map(board, function(row)
+    return "'" .. row .. "', --"
+  end), '\n      ')
+end
+
+return {
+  module_name = 'connect',
+
+  generate_test = function(case)
+    local template = [[
+      local board = {
+        %s
+      }
+      assert.are.same('%s', connect.winner(board))]]
+
+    return template:format(render_board(case.input.board), case.expected)
+  end
+}
